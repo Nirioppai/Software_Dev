8a32cf30551e10a5e7c46cb6f6df823a12909447
Cha-er-si <53701801+Cha-er-si@users.noreply.github.com>
2020-01-16 11:11:21 +0800

    Fixed Fonts

--- resources/views/components/bars.blade.php
+++ resources/views/components/bars.blade.php
@@ -14,6 +14,7 @@
     <link href="{{asset('./img/brand/favicon.png')}}" rel="icon" type="image/png">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
     <!-- Icons -->
     <link href="{{asset('./js/plugins/nucleo/css/nucleo.css')}}" rel="stylesheet" />
     <link href="{{asset('./js/plugins/@fortawesome/fontawesome-free/css/all.min.css')}}" rel="stylesheet" />
