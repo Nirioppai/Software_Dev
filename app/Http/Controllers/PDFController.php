09687c117749e29c46f9dbd7e71f92e77dfefb42
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2020-02-13 11:40:26 +0800

    Finalize PDF by batch

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -52,13 +52,14 @@ class PDFController extends Controller
     {
      $pdf = \App::make('dompdf.wrapper');
      $pdf->loadHTML($this->convert_student_results_to_html($batch));
-     //return $pdf->stream();
-     return $pdf->download('Student Results Batch '.$batch. '.pdf');
+     return $pdf->stream();
+     //return $pdf->download('Student Results Batch '.$batch. '.pdf');
     }
 
     function convert_student_results_to_html($batch)
     {
      $student_results = $this->exportBatch($batch);
+     $date_of_testing = DB::table('student_batch')->where('batch',  $batch)->pluck('exam_date')->first();
      $output = '
      <!DOCTYPE html>
 <html>
@@ -67,34 +68,61 @@ class PDFController extends Controller
       <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body class="body">
-<header>
-    <img style="width: 100%; height: 100%;" src="./img/pdf/PDF_header.png">
-</header>
-     <h3 align="center">STUDENT RESULT BATCH REPORT</h3>
+<img style="width: 100%; height: 100%;" src="./img/pdf/PDF_header.png">
+
+        
+<footer>
+    Legend:
+    <br>
+    1 : Total &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    2 : Verbal &nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    3 : Nonverbal 
+    <br>
+    R : Raw Score &nbsp;&nbsp;&nbsp;&nbsp;
+    S : Scaled Score &nbsp;&nbsp;&nbsp;&nbsp;
+    SA : SAI &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    P : Percentile &nbsp;&nbsp;&nbsp;&nbsp;
+    ST : Stanine &nbsp;&nbsp;&nbsp;&nbsp;
+</footer>
+     
+<main class ="main">
+
+<div>
+
+    <p class="enlarged" style ="padding-left: 4%;">Student Result Batch: '.$batch.' 
+        <br>
+        Date of Testing: '.$date_of_testing. '
+        <br>
+        School: Xavier School San Juan
+<br>
+<br>
+     <h3 align="center">BATCH SUMMARY REPORT</h3>
      <br>
-     <table width="100%" style="border-collapse: collapse; border: 0px;">
-      <tr>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.2%">Student No.</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.3%">Name</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.23%">DOB</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">Grade</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">Age</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1R</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1S</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1SA</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1P</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1ST</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2R</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2S</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2SA</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2P</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2ST</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3R</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3S</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3SA</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3P</th>
-    <th style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3ST</th>
+<table width="100%" style="border-collapse: collapse; border: 0px;">
+<thead>
+    <tr>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.2%">Student No.</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.3%">Name</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.23%">DOB</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">Grade</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">Age</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1R</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1S</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1SA</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1P</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">1ST</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2R</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2S</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2SA</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2P</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">2ST</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3R</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3S</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3SA</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3P</th>
+    <th bgcolor="#e6e5e5" style="border: 1px solid; padding:1px; text-align:center;" width="0.1%">3ST</th>
    </tr>
+   </thead>
      ';  
      foreach($student_results as $student)
      {
@@ -125,9 +153,10 @@ class PDFController extends Controller
      }
      $output .= '</table>
 
+</div>
 
-
-     </body>
+</main>
+</body>
 </html>
 ';
      return $output;
