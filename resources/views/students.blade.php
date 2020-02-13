187e6463006bc7706f136c748bc0488b91df23ed
rjrecierdo <45199571+rafaeljohnrecierdo@users.noreply.github.com>
2020-02-13 11:35:53 +0800

    added pics

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -88,7 +88,7 @@
       <div class="card hoverable">
 
         <!-- Card image -->
-        <img class="card-img-top" src="http://localhost:8000/./img/brand/Slanted-Gradient1.png" alt="Card image cap">
+        <img class="card-img-top" src="http://localhost:8000/./img/brand/bg.jpg" alt="Card image cap">
 
 
 
