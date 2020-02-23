d571836665c346a116e704404e72cba0bd4a1077
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2020-02-23 22:06:10 +0800

    Trimmed unnecessary info

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -58,183 +58,158 @@ class PDFController extends Controller
 
     function convert_student_results_to_html($batch)
     {
-     $student_results = $this->exportBatch($batch);
-     $date_of_testing = DB::table('student_batch')->where('batch',  $batch)->pluck('exam_date')->first();
-     $output = '
-     <!DOCTYPE html>
-<html>
-<head>
-    <link href="./css/pdf.css" rel="stylesheet" />
-      <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-</head>
-<body class="body">
-<img style="width: 100%; height: 100%;" src="./img/pdf/PDF_header.png">
+    $student_results = $this->exportBatch($batch);
+    $date_of_testing = DB::table('student_batch')->where('batch',  $batch)->pluck('exam_date')->first();
+    $output = '
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link href="./css/pdf.css" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+        </head>
+        <body class="body">
+            <img style="width: 100%; height: 100%;" src="./img/pdf/PDF_header.png">      
+            <footer style ="padding-left: 3%;">
+                Legend:
+                <br>
+                1 : Total &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                2 : Verbal &nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                3 : Nonverbal 
+                <br>
+                R : Raw Score &nbsp;&nbsp;&nbsp;&nbsp;
+                S : Scaled Score &nbsp;&nbsp;&nbsp;&nbsp;
+                SA : SAI &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                P : Percentile &nbsp;&nbsp;&nbsp;&nbsp;
+                ST : Stanine &nbsp;&nbsp;&nbsp;&nbsp;
+            </footer>
+            
+        <main class ="main">
+        <div>
+                
+                <h1 align="center">OTIS-LENNON SCHOOL ABILITY TEST 8th Ed. Level F</h1>
+                <h2 align="center">Masterlist Report</h2>
+                <p class="enlarged" style ="padding-left: 3%;">
+                School: Xavier School San Juan
+            
+                <br>
+                Date of Testing: '.$date_of_testing. '
+                <br>
+                Student Result Batch: '.$batch.' 
+                <br>
+        <table width="90%" style="border-collapse: collapse; border: 0px;">
+            <thead>
+                <tr>
+                    <th class="normal-border" width="0.3%" rowspan="4">No.</th>
+                    <th class="normal-border" width="0.3%" rowspan="4">Name</th>
+                    <th class="normal-border" width="0.3%" rowspan="4">Birthday</th>
+                    <th class="normal-border" width="0.3%" rowspan="4">Age</th>
+                    <th class="normal-border" width="0.3%" colspan="8">Verbal</th>
+                    <th class="normal-border" width="0.3%" colspan="8">Non Verbal</th>
+                    <th class="normal-border" width="0.3%" colspan="6">Total Score</th>
+                    <th class="normal-border" width="0.3%" colspan="2" rowspan="3">Normal Curve Equivalent</th>
+                </tr>
 
-        
-<footer>
-    Legend:
-    <br>
-    RS : Raw Score &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    SAI : Scholastic Ability Index &nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;
-    S : Stanine 
-    <br>
-    SS : Scaled Score &nbsp;&nbsp;&nbsp;&nbsp;
-    PR : Percentile Rank &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    CL : Classification &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-</footer>
-     
-<main class ="main">
+                <tr>
+                    <th class="normal-border" width="0.3%" rowspan="1">Verbal Comprehension</th>
+                    <th class="normal-border" width="0.3%" rowspan="1">Verbal Reasoning</th>
+                    <th class="normal-border" width="0.3%" colspan="6">Total Verbal</th>
+                    <!--Verbal-->
+                    <th class="normal-border" width="0.3%">Figural Reasoning</th>
+                    <th class="normal-border" width="0.3%">Quantitative Reasoning</th>
+                    <th class="normal-border" width="0.3%" colspan="6">Total Non Verbal</th>
+                    <!--Non Verbal-->
 
-<div>
+                    <th class="normal-border" width="0.3%" rowspan="3">RS</th>
+                    <th class="normal-border" width="0.3%" rowspan="3">SS</th>
+                    <th class="normal-border" width="0.3%" colspan="4" rowspan="2">Age Norms</th>
+                    
+                </tr>
 
-    <p class="enlarged" style ="padding-left: 4%;">Student Result Batch: '.$batch.' 
-        <br>
-        Date of Testing: '.$date_of_testing. '
-        <br>
-        School: Xavier School San Juan
-<br>
-<br>
-     <h3 align="center">BATCH SUMMARY REPORT</h3>
-     <br>
-<table width="90%" style="border-collapse: collapse; border: 0px;">
-<thead>
-     <tr>
-    <th class="border" width="0.1%" rowspan="4">No.</th>
-    <th class="border" width="0.3%" rowspan="4">Name</th>
-    <th class="border" width="0.3%" rowspan="4">Birthday</th>
-    <th class="border" width="0.3%" rowspan="4">Age</th>
-    <th class="border" width="0.3%" colspan="14" bgcolor="#e6e5e5"> Verbal</th>
-    <th class="border" width="0.3%" colspan="14" bgcolor="#e6e5e5"> Non Verbal</th>
-    <th class="border" width="0.3%" colspan="10" bgcolor="#e6e5e5">Total Score</th>
-    <td class="border" width="0.3%" colspan="2" rowspan="3">Normal Curve Equivalent</td>
+                <tr>
+                    <th class="normal-border" width="0.3%" rowspan="2">RS</th>
+                    <th class="normal-border" width="0.3%" rowspan="2">RS</th>
 
-    
-  </tr>
-  <tr>
-    <td class="border" width="0.3%" colspan="2" rowspan="2">Verbal Comprehension</td>
-    <td class="border" width="0.3%" colspan="2" rowspan="2">Verbal Reasoning</td>
-    <td class="border" width="0.3%" colspan="10">Total Verbal </td>
-    <td class="border" width="0.3%" colspan="2">Figural Reasoning</td>
-    <td class="border" width="0.3%" colspan="2">Quantitative Reasoning</td>
-    <td class="border" width="0.3%" colspan="10">Total Nonverbal</td>
-    <td class="border" width="0.3%" rowspan="3">No. of Items</td>
-    <td class="border" width="0.3%" rowspan="3">RS</td>
-    <td class="border" width="0.3%" rowspan="3">SS</td>
-    <td class="border" width="0.3%" rowspan="2" colspan="3">Grade Norms</td>
-    <td class="border" width="0.3%" rowspan="2" colspan="4">Age Norms</td>
+                    <th class="normal-border" width="0.3%" rowspan="2">RS</th>
+                    <th class="normal-border" width="0.3%" rowspan="2">SS</th>
+                    <th class="normal-border" width="0.3%" colspan="4">Age Norms</th>
+                    <!--Verbal-->
+                    <th class="normal-border" width="0.3%" rowspan="2">RS</th>
+                    <th class="normal-border" width="0.3%" rowspan="2">RS</th>
 
-    
-  </tr>
-  <tr>
-    <td class="border" width="0.3%" rowspan="2">No. of Items</td>
-    <td class="border" width="0.3%" rowspan="2">RS</td>
-    <td class="border" width="0.3%" rowspan="2">SS</td>
-    <td class="border" width="0.3%" colspan="3">Grade Norms</td>
-    <td class="border" width="0.3%" colspan="4">Age Norms</td>
-    <td class="border" width="0.3%" rowspan="2">No. of Items</td>
-    <td class="border" width="0.3%" rowspan="2">RS</td>
-    <td class="border" width="0.3%" rowspan="2">No. of Items</td>
-    <td class="border" width="0.3%" rowspan="2">RS</td>
-    <td class="border" width="0.3%" rowspan="2">No. of Items</td>
-    <td class="border" width="0.3%" rowspan="2">RS</td>
-    <td class="border" width="0.3%" rowspan="2">SS</td>
-    <td class="border" width="0.3%" colspan="3">Grade Norms</td>
-    <td class="border" width="0.3%" colspan="4">Age Norms</td>
+                    <th class="normal-border" width="0.3%" rowspan="2">RS</th>
+                    <th class="normal-border" width="0.3%" rowspan="2">SS</th>
+                    <th class="normal-border" width="0.3%" colspan="4">Age Norms</th>
+                    <!--Non Verbal-->
 
+                    
+                    
+                </tr>
 
-  </tr>
-  <tr>
-    <td class="border" width="0.3%">No. of Items</td>
-    <td class="border" width="0.3%">RS</td>
-    <td class="border" width="0.3%">No. of Items</td>
-    <td class="border" width="0.3%">RS</td>
-    <td class="border" width="0.3%">PR</td>
-    <td class="border" width="0.3%">S</td>
-    <td class="border" width="0.3%">CL</td>
-    <td class="border" width="0.3%">SAI</td>
-    <td class="border" width="0.3%">PR</td>
-    <td class="border" width="0.3%">S</td>
-    <td class="border" width="0.3%">CL</td>
-    <td class="border" width="0.3%">PR</td>
-    <td class="border" width="0.3%">S</td>
-    <td class="border" width="0.3%">CL</td>
-    <td class="border" width="0.3%">SAI</td>
-    <td class="border" width="0.3%">PR</td>
-    <td class="border" width="0.3%">S</td>
-    <td class="border" width="0.3%">CL</td>
-    <td class="border" width="0.3%">PR</td>
-    <td class="border" width="0.3%">S</td>
-    <td class="border" width="0.3%">CL</td>
-    <td class="border" width="0.3%">SAI</td>
-    <td class="border" width="0.3%">PR</td>
-    <td class="border" width="0.3%">S</td>
-    <td class="border" width="0.3%">CL</td>
-    <td class="border" width="0.3%">Age</td>
-    <td class="border" width="0.3%">Grade</td>
-  </tr>
-   </thead>
-     ';
-     $id = 1;  
-     foreach($student_results as $student)
-     {
-      $output .= '
-        <tr>
-          <td class="border">'.$id.'</td>
-          <td class="border" width="2%">'.$student->student_name.'</td>
-          <td class="border" width="1.2%">'.$student->birthday.'</td>
-          <td class="border">'.$student->age_year.".".$student->age_month.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-          <td class="border">'.$id.'</td>
-        </tr>
-      ';
-      $id++;
-     }
-     $output .= '</table>
+                <tr>
+                    <th class="normal-border" width="0.3%">SAI</th>
+                    <th class="normal-border" width="0.3%">PR</th>
+                    <th class="normal-border" width="0.3%">S</th>
+                    <th class="normal-border" width="0.3%">CL</th>
+                    <!--Verbal-->
+                    <th class="normal-border" width="0.3%">SAI</th>
+                    <th class="normal-border" width="0.3%">PR</th>
+                    <th class="normal-border" width="0.3%">S</th>
+                    <th class="normal-border" width="0.3%">CL</th>
+                    <!--Non Verbal-->
+                    <th class="normal-border" width="0.3%">SAI</th>
+                    <th class="normal-border" width="0.3%">PR</th>
+                    <th class="normal-border" width="0.3%">S</th>
+                    <th class="normal-border" width="0.3%">CL</th>
+                    <th class="normal-border" width="0.3%">Age</th>
+                    <th class="normal-border" width="0.3%">Grade</th>
+                </tr>
 
-</div>
+            </thead>
+            ';  
+            foreach($student_results as $student)
+            {
+            $output .= '
+            <tr>
+                <td class="normal-border">1</td>
+                <td class="normal-border">2</td>
+                <td class="normal-border">3</td>
+                <td class="normal-border">4</td>
+                <td class="normal-border">5</td>
+                <td class="normal-border">6</td>
+                <td class="normal-border">7</td>
+                <td class="normal-border">8</td>
+                <td class="normal-border">9</td>
+                <td class="normal-border">10</td>
+                <td class="normal-border">11</td>
+                <td class="normal-border">12</td>
+                <td class="normal-border">13</td>
+                <td class="normal-border">14</td>
+                <td class="normal-border">15</td>
+                <td class="normal-border">16</td>
+                <td class="normal-border">17</td>
+                <td class="normal-border">18</td>
+                <td class="normal-border">19</td>
+                <td class="normal-border">20</td>
+                <td class="normal-border">21</td>
+                <td class="normal-border">22</td>
+                <td class="normal-border">23</td>
+                <td class="normal-border">24</td>
+                <td class="normal-border">25</td>
+                <td class="normal-border">26</td>
+                <td class="normal-border">27</td>
+                <td class="normal-border">28</td>
+            </tr>
+            ';
+            }
+            $output .= '
+        </table>
 
-</main>
-</body>
-</html>
-';
+        </div>
+
+        </main>
+        </body>
+        </html>
+    ';
      return $output;
     }
 
