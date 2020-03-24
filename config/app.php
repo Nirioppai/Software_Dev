38bb4121b711aa3f05307beb8576e97cbe7aff16
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2020-03-24 17:21:24 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -140,6 +140,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         ConsoleTVs\Charts\ChartsServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         
         /*
          * Package Service Providers...
@@ -202,6 +203,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Charts' => ConsoleTVs\Charts\Facades\Charts::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
     'db_fields' => [
